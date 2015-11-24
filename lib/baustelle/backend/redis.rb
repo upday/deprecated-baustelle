fac7affcd81afa927416bc01d1b58aa50a6ce03d
pabloqc <pablo.qc@gmail.com>
2015-11-24 15:08:30 +0100

    Added redis.rb

---
+++ lib/baustelle/backend/redis.rb
@@ -0,0 +1,21 @@
+module Baustelle
+  module Backend
+    class Redis
+      def initialize(name, options, vpc:)
+        @name = name
+        @options = options
+        @vpc = vpc
+      end
+
+      def build(template)
+      end
+
+      def output(template)
+        @options.inject({}) do |output, (key, value)|
+          output[key.to_s] = value
+          output
+        end
+      end
+    end
+  end
+end
