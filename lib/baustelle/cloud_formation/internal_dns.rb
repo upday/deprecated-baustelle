d0109e87f96bd02f19013129876d238e9623a629
Artur Roszczyk <artur.roszczyk@gmail.com>
2016-02-03 17:25:41 +0100

    Prototype of CNAME generation

--- lib/baustelle/cloud_formation/internal_dns.rb
+++ lib/baustelle/cloud_formation/internal_dns.rb
@@ -23,5 +23,25 @@ module Baustelle
         OpenStruct.new(id: 'InternalDNSZone', domain: domain)
       end
     end
+
+
+      def cname(template, zone, name:, target:, ttl: 60)
+        name = Array(name).map(&:underscore).
+               map { |s| s.gsub('_', '-') }.
+               join('.')
+        resource_name = ['InternalDNSZone', name].flatten.
+                        map(&:camelize).join
+        template.resource resource_name,
+                          Type: 'AWS::Route53::RecordSet',
+                          Properties: {
+                            HostedZoneId: zone.id,
+                            Type: 'CNAME',
+                            TTL: ttl,
+                            ResourceNameRecords: [target],
+                            Name: domain = "#{name}.#{zone.domain}"
+                          }
+        domain
+      end
+
   end
 end
