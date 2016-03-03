0c68d4c85c91828c0b66a8198eab57a7e2f40063
Artur Roszczyk <artur.roszczyk@gmail.com>
2016-03-03 11:37:55 +0100

    Set additionaly internal DNS for bastion

--- lib/baustelle/cloud_formation/bastion_host.rb
+++ lib/baustelle/cloud_formation/bastion_host.rb
@@ -3,22 +3,22 @@ module Baustelle
     module BastionHost
       extend self
 
-      def apply(template, config, vpc:, stack_name:, parent_iam_role:)
+      def apply(template, config, vpc:, stack_name:, parent_iam_role:,
+                dns_zone:)
         config.fetch('ami').each do |region, ami|
           template.add_to_region_mapping "BastionAMIs", region, 'global', ami
         end
 
-        user_data = {
-          'ssh_acl_github' => Array(config.fetch('github_ssh_keys')),
-          'dns' => {
-            'zone' => zone_name = config.fetch('dns_zone'),
-            'hostname' => "bastion-#{stack_name.underscore.gsub('_', '-')}"
-          }
-        }.to_yaml
+        user_data = generate_user_data(template, config: config,
+                                       dns_zone: dns_zone,
+                                       stack_name: stack_name)
 
         iam_role = parent_iam_role.inherit("bastion_host", {
-                                             'zone_update' => {
-                                               'resource' => "arn:aws:route53:::hostedzone/#{get_zone_id(zone_name)}",
+                                             'zones_update' => {
+                                               'resource' => user_data.dns_config.map { |dns|
+                                                 zone_id = dns['zone_id'] || get_zone_id(dns['zone'])
+                                                 "arn:aws:route53:::hostedzone/#{zone_id}"
+                                               }.uniq,
                                                'action' => 'route53:ChangeResourceRecordSets'
                                              },
                                              'zones_list' => {
@@ -45,7 +45,7 @@ module Baustelle
                             InstanceType: config.fetch('instance_type'),
                             IamInstanceProfile: template.ref(iam_role.instance_profile_name),
                             ImageId: template.find_in_regional_mapping('BastionAMIs', 'global'),
-                            UserData: Base64.encode64(user_data),
+                            UserData: user_data.encoded,
                             SecurityGroups: [template.ref('GlobalSecurityGroup'),
                                              template.ref('BastionSecurityGroup')]
                           }
@@ -74,6 +74,32 @@ module Baustelle
       # TODO: use AWS SDK to get zone ID
       def get_zone_id(*)
         '*'
+      end
+
+      def generate_user_data(template, config:, dns_zone:, stack_name:)
+        head, tail = {
+          'ssh_acl_github' => Array(config.fetch('github_ssh_keys')),
+          'dns' => dns_config = [
+            {
+              'zone' => dns_zone.domain,
+              'zone_id' => divider = 'INTERNAL_ZONE_ID',
+              'hostname' => "bastion"
+            },
+            {
+              'zone' => config.fetch('dns_zone'),
+              'zone_id' => nil,
+              'hostname' => "bastion-#{stack_name.underscore.gsub('_', '-')}"
+            }
+          ]
+        }.to_yaml.split(divider)
+
+        internal_zone_id = template.ref('InternalDNSZone')
+
+        user_data = {
+          'Fn::Base64' => template.join('', head, internal_zone_id, tail)
+        }
+
+        OpenStruct.new(encoded: user_data, dns_config: dns_config)
       end
     end
   end
