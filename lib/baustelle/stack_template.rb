9dd4a80c8ac87547ac3cbe9be390dc7105a6c7eb
Artur Roszczyk <artur.roszczyk@gmail.com>
2016-03-05 08:46:49 +0100

    Make PARALLEL_EB_UPDATES configurable from the ENV variable

--- lib/baustelle/stack_template.rb
+++ lib/baustelle/stack_template.rb
@@ -1,6 +1,6 @@
 module Baustelle
   class StackTemplate
-    PARALLEL_EB_UPDATES = 4
+    PARALLEL_EB_UPDATES = ENV.fetch('PARALLEL_EB_UPDATES', 4)
 
     def initialize(config)
       @config = config
