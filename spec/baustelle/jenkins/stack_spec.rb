d8b9e51365c76a30db695333b7a7675d4cf988dc
pabloqc <pablo.qc@gmail.com>
2016-06-03 16:06:43 +0200

    Finish test

--- spec/baustelle/jenkins/stack_spec.rb
+++ spec/baustelle/jenkins/stack_spec.rb
@@ -20,27 +20,12 @@ stacks:
       us-east-1: ami-654321
 
 applications:
-  custom_hello_world:
-    stack: ruby2.2-with-datadog
-    instance_type: t2.small
-    scale:
-      min: 1
-      max: 1
-    elb:
-      visibility: internal
   hello_world:
     stack: ruby
     instance_type: t2.small
     scale:
       min: 2
       max: 4
-    config:
-      RAILS_ENV: production
-      RABBITMQ_URL: backend(RabbitMQ:main:url)
-      DATABASE_URL: backend(External:postgres:url)
-      CUSTOM_HELLO_URL: application(custom_hello_world:url)
-      HTTPS_APP_URL: application(https_hello_world:url)
-      OLD_HOSTNAME_SCHEME_APP: application(hello_world_old_hostname_scheme:url)
 
 environments:
   production:
@@ -59,16 +44,6 @@ environments:
       External:
         postgres:
           url: postgres://staging
-  naming:
-    applications:
-      application_default_environment_naming_override:
-        new_environment_naming: true
-      application_compat_environment_naming:
-        new_environment_naming: true
-      application_default_environment_naming:
-        stack: ruby2.2-new-name
-      application_new_environment_naming:
-        stack: ruby2.2-new-name
     YAML
   }
   let(:region){
@@ -91,7 +66,11 @@ environments:
       obj = double()
       test_object = generate_test_object(obj)
       expect(obj).to receive(:create_list_view).with({:name=>"Baustelle test-stackname (eu-west-1)",:regex=>"baustelle-test-stackname-eu-west-1-.*"})
-      expect(obj).to receive(:create_list_view).at_least(:once)
+      expect(obj).to receive(:create_list_view).with({:name=>"Baustelle test-stackname (eu-west-1) production DEPLOY",:regex=>"baustelle-test-stackname-eu-west-1-production-.*-00-deploy"})
+      expect(obj).to receive(:create_list_view).with({:name=>"Baustelle test-stackname (eu-west-1) staging DEPLOY",:regex=>"baustelle-test-stackname-eu-west-1-staging-.*-00-deploy"})
+      expect(obj).to receive(:create_list_view).with({:name=>"Baustelle Hello World",:regex=>"Baustelle .* hello_world .*"})
+
+      #expect(obj).to receive(:create_list_view).at_least(:once)
       test_object.create_views
     end
 
