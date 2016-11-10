c20edc1c13034c1e6c823dec09d139d66f6244a8
Mike Paßberg <mike@upday.com>
2016-11-10 10:38:10 +0100

    Render groovyy definitions

--- lib/baustelle/jenkins/job_template.rb
+++ lib/baustelle/jenkins/job_template.rb
@@ -11,25 +11,26 @@ module Baustelle
       end
 
       def render(prefix: '')
-        groovy_template_path = Dir::Tmpname.make_tmpname(['job', '.groovy'],false).gsub('-','_')
+        groovy_template_path = Dir::mkdir('rendered_jobs', 0755) #Dir::Tmpname.make_tmpname(['job', '.groovy'],false).gsub('-','_')
         groovy_template = File.open(File.join(groovy_scripts_dir, groovy_template_path),'w')
         groovy_template.puts render_groovy
         groovy_template.close
-        Dir.mktmpdir do |output_dir|
-          Dir.chdir(job_dsl_dir) do
-            path = File.join('jobs', File.basename(groovy_template.path))
-            if(system "./gradlew -q xml -Psource=#{path} -PoutputDir=#{output_dir}")
-            else
-              raise Exception.new('Error during job DSL rendering')
-            end
-
-          end
-
-          Dir[File.join(output_dir, "*.xml")].inject({}) do |result, filename|
-            result[prefix + File.basename(filename, '.xml')] = File.read(filename)
-            result
-          end
-        end
+          # Dir.mktmpdir do |output_dir|
+          #   Dir.chdir(job_dsl_dir) do
+          #     path = File.join('jobs', File.basename(groovy_template.path))
+          #     #if(system "./gradlew -q xml -Psource=#{path} -PoutputDir=#{output_dir}")
+          #
+          #     else
+          #       raise Exception.new('Error during job DSL rendering')
+          #   end
+          #
+          # end
+          #
+          # Dir[File.join(output_dir, "*.xml")].inject({}) do |result, filename|
+          #     result[prefix + File.basename(filename, '.xml')] = File.read(filename)
+          #     result
+          # end
+        # end
       end
 
       def render_groovy
