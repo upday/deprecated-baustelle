58d040ac50dd84d952e16d74a8beaddb3839c7e0
Lennard Timm <hi@lenn4rd.io>
2017-08-09 16:06:39 +0200

    Skip deleting Jenkins if not exists

--- lib/baustelle/commands/jenkins/delete.rb
+++ lib/baustelle/commands/jenkins/delete.rb
@@ -9,8 +9,11 @@ module Baustelle
         def call(specification_file, region:, name:)
           config = Baustelle::Config.read(specification_file)
           Aws.config[:region] = region
-          Baustelle::Jenkins::Stack.new(name, region: region, config: config).
-            delete
+
+          return if config['stacks']['jenkins'].nil?
+
+          Baustelle::Jenkins::Stack.new(name, region: region, config: config).delete
+
           puts "Deleted jenkins jobs for #{name} stack in #{region}"
         end
       end
