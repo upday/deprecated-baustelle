52c80d8ef529e28f99ac162248824fc9781f1a32
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2017-12-13 09:12:48 +0100

    Add c5 instance types support.

--- lib/rschema/aws_instance_type.rb
+++ lib/rschema/aws_instance_type.rb
@@ -6,6 +6,7 @@ t2.nano t2.micro t2.small t2.medium t2.large
 m4.large m4.xlarge m4.2xlarge m4.4xlarge m4.10xlarge
 m3.medium m3.large m3.xlarge m3.2xlarge
 c4.large c4.xlarge c4.2xlarge c4.4xlarge c4.8xlarge
+c5.large c5.xlarge c5.2xlarge c5.4xlarge c5.9xlarge c5.18xlarge
 c3.large c3.xlarge c3.2xlarge c3.4xlarge c3.8xlarge
 r3.large r3.xlarge r3.2xlarge r3.4xlarge r3.8xlarge
 r4.large r4.xlarge r4.2xlarge r4.4xlarge r4.8xlarge r4.16xlarge
